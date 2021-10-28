65e54cf3fa9cbd160436970ecd8a876a09e5c391
Henrique Pollefrone <henrique-ps3@hotmail.com>
2021-10-27 22:30:42 -0300

    Second Commit

--- server.js
+++ server.js
@@ -1,3 +1,26 @@
+
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+// APP
+const app = express();
+
+// JS, CSS e IMG
+app.use(express.static(__dirname + '/public'));
+
+// Rotas
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+app.listen(8080,() => console.log('server is running'))
+
+
+
+
+/*
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
@@ -6,6 +29,7 @@ const path = require('path');
 http.createServer((req, res) => {
  
     const file = req.url  === '/' ? 'index.html' : req.url
+
     const filePath = path.join(__dirname, 'public', file);
     const extname = path.extname(filePath)
 
@@ -24,4 +48,4 @@ http.createServer((req, res) => {
         )
     
 }).listen(5000, () => console.log('Server is running'));
-
+*/
